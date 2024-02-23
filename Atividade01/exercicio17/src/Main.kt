3d02a9ed749649b7b8cd2dd13cf7fe7dc6e19109
Yasmin Lima <99838930+whoismin@users.noreply.github.com>
2024-02-22 23:09:18 -0300

    Add files via upload

---
+++ Atividade01/exercicio17/src/Main.kt
@@ -0,0 +1,7 @@
+package fundamentos.controles
+fun main(args: Array<String>){
+    val alunos = arrayListOf("André", "Carla", "Marcos")
+    for((indice, aluno) in alunos.withIndex()){
+        println("$indice - $aluno \n")
+    }
+}
